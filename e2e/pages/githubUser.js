6aa8409eac13bf344be18e1e836f682e915b208b
coreyc <ccleary00@gmail.com>
2017-12-23 14:51:40 -0600

    Take screenshot of repositories tab

--- e2e/pages/githubUser.js
+++ e2e/pages/githubUser.js
@@ -1,23 +1,38 @@
+const cmds = {
+  saveScreenshot: (path, client) => {
+    client.saveScreenshot(path);
+    return this;
+  }
+}
+
 module.exports = {
   url: 'https://www.github.com/coreyc',
+  commands: [cmds],
   elements: {
     pinnedRepos: {
       selector: '.js-pinned-repos-reorder-container'
-    },
-    overview: {
-      selector: 'a[title=Overview]'
-    },
-    repositories: {
-      selector: 'a[title=Repositories]'
-    },
-    stars: {
-      selector: 'a[title=Stars]'
-    },
-    followers: {
-      selector: 'a[title=Followers]'
-    },
-    following: {
-      selector: 'a[title=Following]'
+    }
+  },
+  sections: {
+    userProfileNav: {
+      selector: '.user-profile-nav.js-sticky',
+      elements: {
+        overview: {
+          selector: 'a[title=Overview]'
+        },
+        repositories: {
+          selector: 'a[title=Repositories]'
+        },
+        stars: {
+          selector: 'a[title=Stars]'
+        },
+        followers: {
+          selector: 'a[title=Followers]'
+        },
+        following: {
+          selector: 'a[title=Following]'
+        }
+      }
     }
   }
 };
--- e2e/tests/github-po.js
+++ e2e/tests/github-po.js
@@ -15,13 +15,25 @@ module.exports = {
 
   'Github user page test': (client) => {
     const github = client.page.githubUser();
+    const nav = github.section.userProfileNav;
     github
       .navigate()
       .waitForElementVisible('body', 1000)
       .assert.cssProperty('a', 'color', 'rgba(3, 102, 214, 1)')
-      .waitForElementVisible('@pinnedRepos', 1000)
+      .waitForElementVisible('@pinnedRepos', 1000);
+
+    nav.expect.element('@overview').to.be.visible;
+    nav.expect.element('@repositories').to.be.visible;
+    nav.expect.element('@stars').to.be.visible;
+    nav.expect.element('@followers').to.be.visible;
+    nav.expect.element('@following').to.be.visible;
+    nav
       .click('@repositories')
       .assert.urlContains('repositories');
+
+    github.
+      saveScreenshot('e2e/screenshots/repositories.png', client)
+      
 
     client.end();
   }
